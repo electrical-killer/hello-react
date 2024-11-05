3909bafc4b6f328d2a3a8a4788d6e8ca24884c78
Electrical Killer <e.work.tan@outlook.com>
2024-11-05 21:54:03 +0800

    完成鼠标移动在列表上显示高亮&删除按钮

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,17 +1,28 @@
 import React, { Component } from "react";
 // import item from"./Item.module.css";
 import "./Item.module.css";
+import "../../App.css"
 
 export default class Item extends Component {
+
+    state = {mouse:false}
+
+    headleMouse = flag=>{
+        return ()=>{
+            this.setState({mouse:flag})
+        }
+    }
+
     render() {
         const {title, done} = this.props;
+        const {mouse} = this.state;
         return (
-            <li>
+            <li style={{backgroundColor:mouse?'#ddd':"white"}} onMouseEnter={this.headleMouse(true)} onMouseLeave={this.headleMouse(false)}>
                 <label>
                     <input type="checkbox" defaultChecked={done}/>
                     <span>{title}</span>
                 </label>
-                <button>删除</button>
+                <button className="btn btn-danger" style={{display:mouse?'block':"none"}}>删除</button>
             </li>
         );
     }
